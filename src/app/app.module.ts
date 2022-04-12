5ff14da6b2c533e0835e13f8bfa0a908a25431d4
Thiago Berrutti <thiagoberrutti@gmail.com>
2022-04-12 14:18:44 -0300

    add navbar; created login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent,
+        LoginComponent,
+        NavbarComponent
     ],
-    imports: [
+    imports: [        
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        MatToolbarModule,
+        MatButtonModule,
+        BrowserAnimationsModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
